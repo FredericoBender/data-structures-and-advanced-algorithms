bdab442cb08284522cd06c2616273aa772f1235a
unknown <frederico.bender99@gmail.com>
2020-04-06 15:31:52 -0300

    linked list code completed

--- linkedList.py
+++ linkedList.py
@@ -9,9 +9,18 @@ class LinkedList:
         self.head = None
         self._size = 0
 
+    def _get(self, index):
+        currentValue = self.head
+        for i in range(index):
+            if(currentValue): #Se ele existe
+                currentValue = currentValue.next
+            else:
+                raise IndexError("list index out of range")
+        return currentValue
+
     def append(self, value):
         """Append a value in the list"""
-        if(self.head ):
+        if(self.head):
             pointer = self.head
             while (pointer.next!=None):
                 pointer = pointer.next
@@ -19,36 +28,7 @@ class LinkedList:
         else:
             self.head = Node(value)
         self._size+=1
-
-    def set (self, index):
-        pass
-    def get(self, index):
-        pass
-
-    def __getitem__(self, index): #__ é sobrecarga de operador, da pra usar coisas nativas da linguagem no seu objeto como o []
-        currentValue = self.head
-        for i in range(index):
-            if(currentValue): #Se ele existe
-                currentValue = currentValue.next
-            else:
-                raise IndexError("list index out of range")
-        if(currentValue):
-            return currentValue.value
-        else:
-            raise IndexError("list index out of range")
-
-    def __setitem__(self,index,value):
-        currentValue = self.head
-        for i in range(index):
-            if(currentValue): 
-                currentValue = currentValue.next
-            else:
-                raise IndexError("list index out of range")
-        if(currentValue):
-            currentValue.value = value
-        else:
-            raise IndexError("list index out of range")
-
+    
     def index (self, value):
         """Return index of a value in the list"""
         currentValue = self.head
@@ -60,15 +40,79 @@ class LinkedList:
             i+=1
         raise ValueError("{} is not in list".format(value))
     
+    def insert(self, index, value):
+        node = Node(value)
+        if index == 0:
+            node.next = self.head
+            self.head = node
+        else:
+            pointer = self._get(index-1)
+            node.next = pointer.next
+            pointer.next = node
+        self._size = self._size + 1
+
+    def remove(self, value):
+        if self.head == None:
+            raise ValueError("{} is not in list".format(value))
+        elif self.head.value == value:
+            self.head = self.head.next
+            self._size = self._size - 1
+            return True
+        else:
+            ancestor = self.head
+            pointer = self.head.next
+            while(pointer):
+                if pointer.value == value:
+                    ancestor.next = pointer.next
+                    pointer.next = None
+                    self._size = self._size - 1
+                    return True
+                ancestor = pointer
+                pointer = pointer.next
+        raise ValueError("{} is not in list".format(value))
+
+    def __getitem__(self, index): 
+        currentValue = self._get(index)
+        if(currentValue):
+            return currentValue.value
+        else:
+            raise IndexError("list index out of range")
+
+    def __setitem__(self,index,value):
+        currentValue = self._get(index)
+        if(currentValue):
+            currentValue.value = value
+        else:
+            raise IndexError("list index out of range")
+    
     def __len__(self): #Python function len now will work with this object
         """Return the size of the list"""
         return self._size
+
+    def __repr__(self):
+        r = ""
+        pointer = self.head
+        while(pointer):
+            r = r + str(pointer.value) + "->"
+            pointer = pointer.next
+        return r
+
+    def __str__(self):
+        return self.__repr__()
 
 if (__name__=="__main__"):
     valuesList = LinkedList()
     valuesList.append(4)
     valuesList.append(3)
+
+    valuesList.insert(0,13)
+    valuesList.insert(1,123)
+
+    valuesList[3]=300
+
     print(valuesList[0])
     print(valuesList[1])
-    print(valuesList.index(4))
-    print(len(valuesList))
+
+    #print(valuesList.index(4))
+    #print(len(valuesList))
+    print(valuesList)

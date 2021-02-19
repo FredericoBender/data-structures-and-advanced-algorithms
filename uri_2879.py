660767092ac225de47d878cd3ffb12a30ee9bed5
Frederico Bender <frederico.bender99@gmail.com>
2021-02-19 11:58:05 -0300

    removi arquivos de auxilio

--- uri_2879.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-winsCounter = 0
-
-for i in range(n):
-    currentValue = int(input())
-    if currentValue == 2 or currentValue == 3:
-        winsCounter += 1
-        
-print(winsCounter)
